c79ec913b2e557801ff92e79e6f87d625b83adec
takiyu <takiyu1025.txu.development@gmail.com>
2020-09-11 21:30:55 +0900

    Add async command

--- rplugin/python3/fast_clang_format.py
+++ rplugin/python3/fast_clang_format.py
@@ -17,8 +17,15 @@ class FastClangFormatPlugin(object):
     def __init__(self, nvim):
         self.nvim = nvim
 
-    @neovim.command('FastClangFormat', sync=True, nargs='?')
-    def make_split_comment(self, args):
+    @neovim.command('FastClangFormat', sync=True, nargs=0)
+    def apply_clang_format(self, args):
+        self._impl()
+
+    @neovim.command('FastClangFormatAsync', sync=False, nargs=0)
+    def apply_clang_format_async(self, args):
+        self._impl()
+
+    def _impl(self):
         # Obtain full path of the current filename
         filename = self.nvim.eval("expand('%:p')")
         # Apply clang-format
